d053802ce819f0ab00d4036ab46204f819588cdd
KHUSHI 444 <khankhushi4452@gmail.com>
2024-12-01 14:34:03 +0530

    images added to the page

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,5 +1,22 @@
-// src/sections/Header.tsx
+import LogoIcon from "../assets/Logo.jpg"
+
 export function Header() {
-   return <header>Header Section</header>;
+   return (
+     <header className="py-4">
+       <div className="container">
+         <div className="flex justify-between ">
+           <div>
+            <LogoIcon className=""/>
+           </div>
+           <div className="flex items-center space-x-4">
+             <button className="px-4 py-2 bg-blue-500 text-white rounded-md">
+               Join Waitlist
+             </button>
+             <span>Menu</span>
+           </div>
+         </div>
+       </div>
+     </header>
+   );
  }
  
