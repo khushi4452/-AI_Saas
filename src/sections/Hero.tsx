4666211172eeade84c2d17aaa51e07d6d258bf86
KHUSHI 444 <khankhushi4452@gmail.com>
2024-12-02 15:50:45 +0530

    transparent css

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,11 +1,18 @@
+import { Button } from "../components/Button";
+
 export const Hero = () => {
-    return <section>
-        <div className="container">
-           <h1>AI SEO</h1>
-           <p>Evaluate your site Visibility effortlessly with AI, where smart technology meets 
-             user-friendly SEO Tools
-              </p>
-              <button> Join Waitlist</button>
-            </div>
-            </section>
+  return (
+    <section>
+      <div className="container">
+        <h1 className="text-8xl font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">
+          AI SEO
+        </h1>
+        <p>
+          Evaluate your site visibility effortlessly with AI, where smart technology meets
+          user-friendly SEO tools.
+        </p>
+        <Button>Join Waitlist</Button>
+      </div>
+    </section>
+  );
 };
