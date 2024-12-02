2ad81c69ad1d0ad06b3f54ed2371e041b3a12481
KHUSHI 444 <khankhushi4452@gmail.com>
2024-12-02 15:22:48 +0530

    button props created

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -25,16 +25,7 @@ export function Header() {
          
           <div className="flex gap-4 items-center">
             
-            <button className="relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#48208a] shadow-[0px_0px_12px_#8cff]">
-              <div className="absolute inset-0">
-                <div className="rounded-lg border border-white/20 absolute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)]">
-                  <div className="border absolute inset-0 border-white/40 [mask-image:linear-gradient(to_top,black,transparent)]"></div>
-                  <div className="absolute inset-0 shadow-[0_0_10px_rgb(140,69,255,.7)_inset] rounded-lg"></div>
-                </div>
-              </div>
-              <span>Join Waitlist</span>
-            </button>
-
+            
           
             <Image src={MenuIcon} alt="Menu" className="h-8 w-8 object-contain sm:hidden" />
           </div>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,7 +2,10 @@ export const Hero = () => {
     return <section>
         <div className="container">
            <h1>AI SEO</h1>
-           <p>Evaluate your site Visibility effortlessly with AI, where smart Technology meets  user-friendly SEO Tools </p>
+           <p>Evaluate your site Visibility effortlessly with AI, where smart technology meets 
+             user-friendly SEO Tools
+              </p>
+              <button> Join Waitlist</button>
             </div>
             </section>
 };
