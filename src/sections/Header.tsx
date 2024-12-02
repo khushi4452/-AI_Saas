60e65dc98b52f9202f3ec7a944490cb7297b91cc
KHUSHI 444 <khankhushi4452@gmail.com>
2024-12-02 14:07:43 +0530

    basic Navbar has been created

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -11,12 +11,20 @@ export function Header() {
             {/* Using Next.js Image component for the logo */}
             <Image src={LogoIcon} alt="Logo" className="h-8 w-8 object-contain" />
           </div>
+          <div>
+            <nav className="flex gap-8 text-white/70">
+              <a href="#">Features</a>
+              <a href="#">Developer</a>
+              <a href="#">pricing</a>
+              <a href="#">changelog</a>
+          
+            </nav>
+          </div>
           <div className="flex gap-4 items-center  ">
             <button className=" relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#48208a] shadow-[0px_0px_12px_#8cff]" >
              <div className="absolute inset-0">
               
                <div className=" rounded-lg border border-white/20 absolute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)]">
-               
                <div className="border absolute inset-0 border-white/40 [mask-image:linear-gradient(to_top,black,transparent)]"></div>
               <div className="absolute inset-0 shadow-[0_0_10px_rgb(140,69,255,.7)_inset] rounded-lg"></div>
                </div>
