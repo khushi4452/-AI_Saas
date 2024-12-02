c7deb8523ad5e65f5ff8070aed01d9145ad4e7a0
KHUSHI 444 <khankhushi4452@gmail.com>
2024-12-02 15:38:05 +0530

    basic changes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-const Button = (props: React.PropsWithChildren) =>{
+ export const Button = (props: React.PropsWithChildren) =>{
   return (
     <button className="relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#48208a] shadow-[0px_0px_12px_#8cff]">
               <div className="absolute inset-0">
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,18 +1,19 @@
 import Image from "next/image";
 import LogoIcon from "../assets/Logo.jpg";
-import MenuIcon from "../assets/Menu1.png"; 
+import MenuIcon from "../assets/Menu1.png";
+import { Button } from "../components/Button";
 
 export function Header() {
   return (
     <header className="py-4 border-b border-white/15 md:border-none">
       <div className="container mx-auto">
         <div className="flex justify-between items-center border-white/15">
-          
+          {/* Logo */}
           <div className="border-white/15 h-10 w-10 rounded-xl inline-flex justify-center items-center md:p-2.5">
             <Image src={LogoIcon} alt="Logo" className="h-8 w-8 object-contain" />
           </div>
 
-          
+          {/* Navigation */}
           <div className="hidden md:block">
             <nav className="flex gap-8 text-sm">
               <a href="#" className="text-white/70 hover:text-white transition">Features</a>
@@ -22,11 +23,9 @@ export function Header() {
             </nav>
           </div>
 
-         
+          {/* Button and Hamburger Menu */}
           <div className="flex gap-4 items-center">
-            
-            
-          
+            <Button>Join Waitlist</Button>
             <Image src={MenuIcon} alt="Menu" className="h-8 w-8 object-contain sm:hidden" />
           </div>
         </div>
