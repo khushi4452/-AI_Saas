c18e639224d695a5906d0080856d47046d9ec84e
KHUSHI 444 <khankhushi4452@gmail.com>
2024-12-06 10:35:15 +0530

    content added

--- src/sections/callToAction.tsx
+++ src/sections/callToAction.tsx
@@ -4,10 +4,10 @@ export const CallToAction = () => {
   return (
     <section className="py-20" >
     <div className="container">
-    <div className="border border-white/15 py-24 rounded-xl" >
+    <div className="border border-white/15 py-24 rounded-xl overflow-hidden relative" >
     
 
-
+    <div className="absolute inset-0 bg-[rgb(74,32,138)]"></div>
     <h2 className="text-5xl tracking-tighter text-center font-medium">AI-Driven SEO for everyone</h2>
     <p className="text-center text-lg text-white/70 px-4 mt-5 tracking-tight">Achieve clear , impactful results without the commplexity</p>
     <div className="glex justify-center mt-8">
